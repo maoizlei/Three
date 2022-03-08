eda69d464168415d7c5277a5a8c49677d837f821
MaoZilei <zilei.mao@hand-china.com>
2022-03-08 15:12:26 +0800

    SignalR

---
+++ SinglR/Controllers/CountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinglR.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/count")]
+    public class CountController:Controller
+    {
+        private readonly IHubContext<CountHub> _countHub;
+        public CountController(IHubContext<CountHub> countHub)
+        {
+            _countHub = countHub;
+        }
+
+        public async Task<IActionResult> Post()
+        {
+            await _countHub.Clients.All.SendAsync("someFunc", new { random = "abcd" });
+            return Accepted(1);
+        }
+    }
+}
---
+++ SinglR/CountHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SinglR.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SinglR
+{
+    [Authorize]
+    public class CountHub:Hub
+    {
+        private readonly CountService _countService;
+        public CountHub(CountService countService)
+        {
+            _countService = countService;
+        }
+
+        public async Task GetLastestCount(string randoms)
+        {
+            var user = Context.User.Identity.Name;
+            int count;
+            do
+            {
+                count = _countService.GetLatestCount();
+                Thread.Sleep(1000);
+                await Clients.All.SendAsync("ReceiveUpdate", count);
+            } while (count < 10);
+            await Clients.All.SendAsync("Finished");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //var connectionId = Context.ConnectionId;
+            //var clients = Clients.Client(connectionId);
+
+            //await clients.SendAsync("someFunc",new { });
+            //await Clients.AllExcept(connectionId).SendAsync("someFunc");
+
+            //await Groups.AddToGroupAsync(connectionId, "MyGroup");
+            //await Groups.RemoveFromGroupAsync(connectionId, "MyGroup");
+
+            //await Clients.Groups("MyGroup").SendAsync("someFunc");
+        }
+    }
+}
